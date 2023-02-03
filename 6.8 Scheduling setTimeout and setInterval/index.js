b0207f15670637d6f0eed5d7c4bfb0222066d20c
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-02-03 15:30:56 +0500

    Scheduling setTimeout and setInterval

--- 6.8 Scheduling setTimeout and setInterval/index.js
+++ 6.8 Scheduling setTimeout and setInterval/index.js
@@ -1,28 +1,39 @@
 function sayHi() {
-    alert('Hello');
+    console.log('Hello');
 }
 setTimeout(sayHi, 1000);
 
 function sayHi(phrase, who) {
-    alert(phrase + ', ' + who);
+    console.log(phrase + ', ' + who);
 }
 setTimeout(sayHi, 1000, "Hello", "John"); // Hello, John
 
-setTimeout("alert('Hello')", 1000);
+setTimeout("console.log('Hello')", 1000);
 
-setTimeout(() => alert('Hello'), 1000);
+setTimeout(() => console.log('Hello'), 1000);
 
-let timerId = setTimeout(() => alert("never happens"), 1000);
-alert(timerId); // timer identifier
+let timerId = setTimeout(() => console.log("never happens"), 1000);
+console.log(timerId); // timer identifier
 clearTimeout(timerId);
-alert(timerId); // same identifier (doesn't become null after canceling)
+console.log(timerId); // same identifier (doesn't become null after canceling)
 
 // repeat with the interval of 2 seconds
-timerId = setInterval(() => alert('tick'), 2000);
+let timerId2 = setInterval(() => console.log('tick'), 2000);
 // after 5 seconds stop
-setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+setTimeout(() => { clearInterval(timerId2); console.log('stop'); }, 5000);
 
-timerId = setTimeout(function tick() {
-    alert('tick');
-    timerId = setTimeout(tick, 2000); // (*)
-  }, 2000);
+let timerId3 = setTimeout(function tick() {
+    console.log('tick');
+    timerId3 = setTimeout(tick, 2000); // (*)
+}, 2000);
+
+let i = 1;
+setInterval(function () {
+    func(i++);
+}, 100);
+
+i = 1;
+setTimeout(function run() {
+    func(i++);
+    setTimeout(run, 100);
+}, 100);
