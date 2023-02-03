58b42d9b5aad046834883fd2f8eb7a29d8f869f2
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-02-03 14:35:14 +0500

    Function object, NFE

--- 6.6 Function object, NFE/index.js
+++ 6.6 Function object, NFE/index.js
@@ -1,15 +1,15 @@
 function sayHi() {
-    alert("Hi");
+    console.log("Hi");
 }
-alert(sayHi.name); // sayHi
+console.log(sayHi.name); // sayHi
 
 let sayHii = function () {
-    alert("Hi");
+    console.log("Hi");
 };
-alert(sayHii.name); // sayHi
+console.log(sayHii.name); // sayHi
 
 function f(sayHi = function () { }) {
-    alert(sayHi.name); // sayHi (works!)
+    console.log(sayHi.name); // sayHi (works!)
 }
 f();
 
@@ -21,33 +21,33 @@ let user = {
         // ...
     }
 }
-alert(user.sayHi.name); // sayHi
-alert(user.sayBye.name); // sayBye
+console.log(user.sayHi.name); // sayHi
+console.log(user.sayBye.name); // sayBye
 
 let arr = [function () { }];
-alert(arr[0].name);   // <empty string>
+console.log(arr[0].name);   // <empty string>
 
 function f1(a) { }
 function f2(a, b) { }
 function many(a, b, ...more) { }
 
-alert(f1.length); // 1
-alert(f2.length); // 2
-alert(many.length); // 2
+console.log(f1.length); // 1
+console.log(f2.length); // 2
+console.log(many.length); // 2
 
 function sayHiii() {
-    alert("Hi");
+    console.log("Hi");
     sayHiii.counter++;
 }
 sayHiii.counter = 0;
 sayHiii(); // Hi
 sayHiii(); // Hi
 
-alert(`Called ${sayHiii.counter} times`); // Called 2 times
+console.log(`Called ${sayHiii.counter} times`); // Called 2 times
 
 let sayHi = function func(who) {
     if (who) {
-        alert(`Hello, ${who}`);
+        console.log(`Hello, ${who}`);
     } else {
         func("Guest"); // use func to re-call itself
     }
@@ -57,7 +57,7 @@ sayHi(); // Hello, Guest
 
 let sayHi = function func(who) {
     if (who) {
-        alert(`Hello, ${who}`);
+        console.log(`Hello, ${who}`);
     } else {
         func("Guest"); // Now all fine
     }
